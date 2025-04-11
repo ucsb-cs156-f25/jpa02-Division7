b2e1c9a69e957e5434b660bbe3f414c942e258ed
Yue Cao <yuecao365@ucsb.edu>
2025-04-10 20:26:09 -0700

    add tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -5,6 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
 public class TeamTest {
 
     Team team;
@@ -20,7 +27,20 @@ public class TeamTest {
     }
 
    
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    public void checkFuntion(){
+        Team team1 = new Team("team1");
+        ArrayList<String> members = new ArrayList<>(List.of("Yue", "Perry", "Lucas"));
+        team1.setMembers(members);
+
+        Team team2 = new Team("team1");
+        team2.setMembers(members);
+
+        assertEquals(team1, team2);
+        assertEquals(members, team.getMembers());
+        assertEquals(team1.hashCode(), team2.hashCode());
+
+        assertNotEquals(team1, new Team());
+        assertEquals(team1.toString(), team1.toString());
+    }
 
 }
